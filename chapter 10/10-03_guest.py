262cb9294eb3a36a57094b787547541a4f0877ec
Kevin <39689243+kjh9000@users.noreply.github.com>
2021-01-31 09:47:16 -0800

    Delete chapter 10 directory

--- chapter 10/10-03_guest.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""Write a program that prompts the user for their name When they
-respond, write their name to a file called guest.txt """
-
-name = input("What name do you want to add to the guest list? ")
-with open('guests.txt', 'a') as fo:
-    fo.write(name)
-    fo.write("\n")
--- chapter 10/10-3_guest.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-guest = input("Whpo is the guest?: ")
-with open('guests.txt', 'a') as fo:
-    fo.write(guest + "\n")
