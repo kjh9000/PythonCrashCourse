c1467cad03d3a51efcf2a4d6cee88cda41b74ab6
Kevin <39689243+kjh9000@users.noreply.github.com>
2021-01-31 09:48:09 -0800

    Delete chapter 8 directory

--- chapter 8/8-01_display_message.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-'''write a function called display_message that prints out a sentence telling
-everyone wht youre learning this chapter
-'''
-
-def display_message():
-    print("I'm learning about functions.")
-display_message()
--- chapter 8/8-15_printing_models.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import print_models
-
-unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
-completed_models = []
-print_models.print_models(unprinted_designs, completed_models)
-print_models.show_completed_models(completed_models)
