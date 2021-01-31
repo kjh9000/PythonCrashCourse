c605a87d0d2e0dadcce8cde7958fa016e250033e
Kevin <39689243+kjh9000@users.noreply.github.com>
2021-01-31 09:48:22 -0800

    Delete chapter 7 directory

--- chapter 7/7-01_rental_cat.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-""" write a program that asks the user what kind of rental car they want. print
-a message about the car, such as 'Let me see if we have a suburu'
-"""
-
-rental = input("What kind of car would you like to rent? ")
-print("Let me see if we have a " + rental.title() + ".")
