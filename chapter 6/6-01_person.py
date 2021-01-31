19d724df35efce99eaacc1bcfdf8f763c849e0ba
Kevin <39689243+kjh9000@users.noreply.github.com>
2021-01-31 09:48:34 -0800

    Delete chapter 6 directory

--- chapter 6/6-01_person.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""store info on someone in a dict. first and last name and where they live.
-use keys first_name, last_name, and city. print each piece of info out
-"""
-
-person = {'first_name': 'Mike', 'last_name': 'johnson', 'city': 'las vegas'}
-
-print(person['first_name'],person['last_name'],'lives in the city of', person['city'] + '.')
