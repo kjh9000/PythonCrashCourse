264c0ee8760bc508a355a830a1fada118ab050cb
kevin <kevinh@programmer.net>
2021-02-08 15:05:17 -0800

    Chapter 04 exercises

--- chapter 04/4-04_count_million.py
+++ chapter 04/4-04_count_million.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 #create a list of numbers from one to one million. use a for loop to print out each number. end the program if its taking too long
-numbers=[i+1 for i in range(0,1000000)]
+numbers=[i+1 for i in range(1000000)]
 for i in numbers:
 	print(i)
