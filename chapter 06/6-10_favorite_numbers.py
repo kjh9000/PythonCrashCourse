91c1b4849ae8deb68a5b0e77418672fe28403a69
kevin <kjh9000@mail.com>
2021-02-09 04:43:46 -0800

    Chapter 06 exercises

--- chapter 06/6-10_favorite_numbers.py
+++ chapter 06/6-10_favorite_numbers.py
@@ -4,7 +4,8 @@
 through the dict and print each person's name and numbers
 """
 
-people = {'larry': [4, 11], 'curly': [12, 41], 'moe': [12, 11], 'bob': [1, 33], 'alice': [55, 12]}
+people = {'larry': [4, 11], 'curly': [12, 41], 'moe': [12, 11], 'bob': [1, 33],
+ 'alice': [55, 12]}
 
 for k, v in people.items():
     print(k.title() + "'s favorite number are " + str(v))
